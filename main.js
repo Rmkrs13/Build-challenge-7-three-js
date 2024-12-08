f0e57be88aad1917bf54126da049463a192f1275
Lars Raeymaekers <lars@rmkrs.be>
2024-12-08 23:05:58 +0100

    Update

--- main.js
+++ main.js
@@ -1,9 +1,8 @@
-import { ChakraUIVuePlugin } from '@chakra-ui/vue-next'
-import '@chakra-ui/vue-next/dist/style.css'
-
 import { createApp } from 'vue'
 import App from './App.vue'
+import { ChakraProvider } from '@chakra-ui/vue-next'
+import '@chakra-ui/vue-next/dist/style.css'
 
 const app = createApp(App)
-app.use(ChakraUIVuePlugin)
+app.use(ChakraProvider)
 app.mount('#app')
