d1930b3eb7a908693db4627757011e5c5591e7b5
Lars Raeymaekers <lars@rmkrs.be>
2024-12-08 16:16:15 +0100

    Cleaning up file strcuture

--- router/index.js
+++ router/index.js
@@ -1,12 +1,10 @@
-import { createRouter, createWebHistory } from "vue-router";
-import Configurator from "../components/Configurator.vue";
-import LoginForm from "../components/LoginForm.vue";
-import Dashboard from "../components/Dashboard.vue";
+import { createRouter, createWebHistory } from 'vue-router';
+import Login from '../views/Login.vue';
+import Configurator from '../views/Configurator.vue';
 
 const routes = [
-  { path: "/", component: Configurator, name: "home" },
-  { path: "/login", component: LoginForm, name: "login" },
-  { path: "/dashboard", component: Dashboard, name: "dashboard" },
+  { path: '/login', name: 'Login', component: Login },
+  { path: '/configurator', name: 'Configurator', component: Configurator },
 ];
 
 const router = createRouter({
