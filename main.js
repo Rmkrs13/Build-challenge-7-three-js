7f0941684c29a2a74b8416358e68d8e2192ec634
Lars Raeymaekers <lars@rmkrs.be>
2024-12-08 23:00:35 +0100

    Install Chakra UI

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import router from "./router";
+import { ChakraUIVuePlugin } from '@chakra-ui/vue-next'
+import '@chakra-ui/vue-next/dist/style.css'
 
-createApp(App).use(router).mount("#app");
+import { createApp } from 'vue'
+import App from './App.vue'
+
+const app = createApp(App)
+app.use(ChakraUIVuePlugin)
+app.mount('#app')
